c69e24e586ee2957284fa4ddc1129969607b7cd8
bruteforce10 <audifirdi@gmail.com>
2026-08-26 10:06:53 +0700

    feat(config): add assetPrefix to match basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/gifts",
+  assetPrefix: "/gifts",
   reactStrictMode: true,
   experimental: {
     serverActions: {
