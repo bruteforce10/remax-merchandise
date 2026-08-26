be9e7b93105f38852feb09464649fdf4854ab947
bruteforce10 <audifirdi@gmail.com>
2026-08-26 10:32:31 +0700

    feat(config): remove assetPrefix from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/gifts",
-  assetPrefix: "/gifts",
   reactStrictMode: true,
   experimental: {
     serverActions: {
